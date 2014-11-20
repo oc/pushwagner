62ce6edeede8eae94dc8a4acc367f8032cb16c94
Ole Christian Rynning <oc@rynning.no>
2014-11-20 11:42:52 +0100

    Support absolute paths for static files

--- lib/pushwagner/static.rb
+++ lib/pushwagner/static.rb
@@ -14,10 +14,11 @@ module Pushwagner
         environment.static.each do |name, files|
           environment.hosts.each do |host|
             Net::SCP.start(host, environment.user) do |scp|
-              puts "Uploading files to #{host}:#{environment.path_prefix}/#{name}/"
+              dest = name.start_with?('/') ? name : "#{environment.path_prefix}/#{name}/"
+              puts "Uploading files to #{host}:#{dest}/"
               files.each do |f|
                 if File.exists?(f)
-                  scp.upload!(f, "#{environment.path_prefix}/#{name}/", :recursive => File.directory?(f))
+                  scp.upload!(f, dest, :recursive => File.directory?(f))
                 else
                   puts "Warning: File #{f} does not exist"
                 end
